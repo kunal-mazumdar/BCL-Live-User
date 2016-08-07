bc1392c546700d3c7cb60d19431879223a508c69
KunalMZ <kunal_mazumdar@outlook.com>
2016-08-07 12:19:39 +0530

    dashboard fixed

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -11,56 +11,70 @@ angular.module('bclLiveApp')
   .controller('dashboardCtrl', function($scope, $firebaseObject, $firebaseArray, fbServ){
     $scope.refDashboard = new Firebase(atob(fbServ.fbob)+'/dashboard');
     var obj = $firebaseObject($scope.refDashboard);
-    obj.$bindTo($scope, 'dashboard');
+    obj.$bindTo($scope, 'dashboard').then(function(){
 
-    $scope.refFB = new Firebase(atob(fbServ.fbob)+"/fixtures");
-    var arr = $firebaseArray($scope.refFB);
+      $scope.refFB = new Firebase(atob(fbServ.fbob)+"/fixtures");
+      var arr = $firebaseArray($scope.refFB);
 
-    $scope.getTotalRuns = function(){
-      var totalRuns = 0;
-      for(var i in arr){
-        if(arr[i]['team1'] || arr[i]['team2']){
-          totalRuns += arr[i].team1.totalRunsScored;
-          totalRuns += arr[i].team2.totalRunsScored;
-          break;
+      $scope.getTotalRuns = function(){
+        var totalRuns = 0;
+        for(var i in arr){
+          if(arr[i]['team1'] || arr[i]['team2']){
+            if(arr[i].team1.totalRunsScored){
+              totalRuns += arr[i].team1.totalRunsScored;
+            }
+            if(arr[i].team2.totalRunsScored){
+              totalRuns += arr[i].team2.totalRunsScored;
+            }
+          }
         }
+        return totalRuns;
       }
-      return totalRuns;
-    }
 
-    $scope.getTotalWickets = function(){
-      var totalWickets = 0;
-      for(var i in arr){
-        if(arr[i]['team1'] || arr[i]['team2']){
-          totalWickets += arr[i].team1.totalWicketsLost;
-          totalWickets += arr[i].team2.totalWicketsLost;
-          break;
+      $scope.getTotalWickets = function(){
+        var totalWickets = 0;
+        for(var i in arr){
+          if(arr[i]['team1'] || arr[i]['team2']){
+            if(arr[i].team1.totalWicketsLost){
+              totalWickets += arr[i].team1.totalWicketsLost;
+            }
+            if(arr[i].team2.totalWicketsLost){
+              totalWickets += arr[i].team2.totalWicketsLost;
+            }
+          }
         }
+        return totalWickets;
       }
-      return totalWickets;
-    }
 
-    $scope.getTotalFours = function(){
-      var totalFours = 0;
-      for(var i in arr){
-        if(arr[i]['team1'] || arr[i]['team2']){
-          totalFours += arr[i].team1.totalFours;
-          totalFours += arr[i].team2.totalFours;
-          break;
+      $scope.getTotalFours = function(){
+        var totalFours = 0;
+        for(var i in arr){
+          if(arr[i]['team1'] || arr[i]['team2']){
+            if(arr[i].team1.totalFours){
+              totalFours += arr[i].team1.totalFours;
+            }
+            if(arr[i].team2.totalFours){
+              totalFours += arr[i].team2.totalFours;
+            }
+          }
         }
+        return totalFours;
       }
-      return totalFours;
-    }
 
-    $scope.getTotalSixes = function(){
-      var totalSixes = 0;
-      for(var i in arr){
-        if(arr[i]['team1'] || arr[i]['team2']){
-          totalSixes += arr[i].team1.totalSixes;
-          totalSixes += arr[i].team2.totalSixes;
-          break;
+      $scope.getTotalSixes = function(){
+        var totalSixes = 0;
+        for(var i in arr){
+          if(arr[i]['team1'] || arr[i]['team2']){
+            if(arr[i].team1.totalSixes){
+              totalSixes += arr[i].team1.totalSixes;
+            }
+            if(arr[i].team2.totalSixes){
+              totalSixes += arr[i].team2.totalSixes;
+            }
+          }
         }
+        return totalSixes;
       }
-      return totalSixes;
-    }
+
+    });
   });
